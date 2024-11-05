a2e79ffd4cdc4a6e0e6de41e5ecb79a92eecee99
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-11-04 21:08:26 -0300

    :test_tube: new tests added

---
+++ src/iaclient/tests/test_client.py
@@ -0,0 +1,36 @@
+import os
+os.environ["PROMPTS_PATH"] = "src/iaclient/tests/prompt"
+from ..promptInfo import PromptInfo
+from ..client import IAClient
+from ...comment import Comment
+from ..responseInfo import ResponseInfo
+from typing import List
+
+class FakeClient(IAClient):
+
+    def __init__(self):
+        super().__init__("test-client")
+        self.countReqs = 0
+
+    def _separateCommentsBatch(self,comments: List[Comment]) -> List[List[Comment]]:
+        lot1 = comments[::2]
+        lot2 = comments[1::2]
+        return [lot1,lot2]
+    def _generatePrompt(self,comments: List[Comment]):
+        assert len(comments) == 3, "expected only 3 comments after being separated"
+        firstCmt = comments[0]
+        return PromptInfo('test1' if str(firstCmt) == 'prompt1' else 'test2')
+    def _makeRequestToAi(self,comments: List[Comment],prompt:str)->ResponseInfo:
+        self.countReqs+=1
+        assert len(comments) == 3
+        if(self.countReqs > 2):
+            raise Exception('Expected only 2 requests to AI')
+        assert prompt.startswith('<FIRST_TEST>') if self.countReqs == 1 else prompt.startswith('<SECOND_TEST>')
+        response = ResponseInfo()
+        response.setData([{"behavior":"neutral"},{"behavior":"happy"},{"behavior":"angry"}])
+        return response
+def test_client_flux():
+    client = FakeClient()
+    comments = [Comment(x,'def','comment') for x in range(6)]
+    comments[0].message = "prompt1"
+    assert client.analyze(comments)
--- src/tests/test_comment.py
+++ src/tests/test_comment.py
@@ -2,11 +2,41 @@ from ..comment import Comment
 
 def test_generate_simple_dict():
     _id = 1
-    message = "msgTest"
+    message = 'msgTest'
     msgType = 'comment'
     comment = Comment(_id,message,msgType)
     inList = dict(comment)
-    expected = {
+    expected = makeBasicDict(_id,message,msgType)
+    assert inList == expected, "Comment don't generate the expected dict"
+
+def test_generate_dict_with_process_info():
+    from time import time
+    _id = 1
+    message = 'msgTest'
+    msgType = 'comment'
+
+    prcName = 'testProcess/1.0'
+    fakeHash = 'fawqh'
+    comment = Comment(_id,message,msgType)
+    comment.attachInfo({'spam':False},prcName,fakeHash)
+    expected = makeBasicDict(_id,message,msgType)
+    expected['data'] = {'spam':False}
+    expected['process'] = [{
+        'name':prcName,
+        'hash':fakeHash,
+        'timestamp':int(time() * 1000)
+    }]
+    assert expected == dict(comment)
+    
+def test_get_message_using_str():
+    _id = 1
+    message = 'msgTest'
+    msgType = 'comment'
+    comment = Comment(_id,message,msgType)
+    assert message == str(comment)
+
+def makeBasicDict(_id = 1,message = 'msgTest',msgType = 'comment'):
+    return {
         "id":_id,
         "message":message,
         "type":msgType,
@@ -14,4 +44,3 @@ def test_generate_simple_dict():
         "process":[],
         "data":{}
     }
-    assert inList == expected, "Comment don't generate the expected dict"
