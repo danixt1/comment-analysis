47138c61373d4fd160ecc2867337d7a6c5dc97ce
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-11-11 13:20:06 -0300

    :bug: fix not calling initInstanceables/ incorrect item varname

--- src/output/outputManager.py
+++ src/output/outputManager.py
@@ -1,11 +1,14 @@
 from .outputBase import OutputBase
 from src.managerBase import ManagerBase
-
+from .managerInstanceable import initInstanceables
 class OutputManager(ManagerBase):
     
     def __init__(self):
-        self.items: list[OutputBase] = []
+        super().__init__()
+        self._items: list[OutputBase] = []
     
     def output(self,comments):
-        for output in self.items:
+        for output in self._items:
             output.sendData(comments)
+    
+initInstanceables(OutputManager)
