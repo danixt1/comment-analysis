70e08379408b735a90acbff58bb77cb7ff544a15
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-11-27 15:19:49 -0300

    :sparkles: feat: new cache system

--- src/cache.py
+++ src/cache.py
@@ -9,54 +9,89 @@ def makeHash(f):
 
 class Cache:
     timestamp =int(datetime.now().timestamp())
-    def __init__(self,subpath:str):
-        self.cache = {}
-        path = Path("cache")
-        path.mkdir(exist_ok=True) 
-        path = path / subpath
-        path.mkdir(exist_ok=True)    
-        self.path = path
 
-    def _makeCache(self, fromConfig):
-        hashValue = makeHash(json.dumps(fromConfig, sort_keys=True))
-        path = self.path / f"{self.timestamp}-{hashValue}.json"
+    def __init__(self,subpath:str = None,prefix = None,actPath = None):
+        self.cache = {}
+        self.prefix = prefix
+        self.n = 0
+        if actPath:
+            self.path = Path(actPath)
+            return
+
+        path = Path("cache")
+        if subpath:
+            path.mkdir(exist_ok=True) 
+            path = path / subpath
+            path.mkdir(exist_ok=True)    
+        self.path = path
+    def cacheWithPrefix(self,prefix):
+        if isinstance(prefix, (dict, list)):
+            prefix =makeHash(json.dumps(prefix, sort_keys=True))
+        return Cache(prefix=prefix,actPath=self.path)
+    def _makeCache(self, fromInput):
+        filename = str(self.timestamp)
+        if self.prefix:
+            filename += f"-{self.prefix}"
+        if fromInput:
+            hashValue = makeHash(json.dumps(fromInput, sort_keys=True))
+            filename += f"-{hashValue}"
+        else:
+            filename += f"-{self.n}"
+            self.n += 1
+        path = self.path / filename
         path.touch(exist_ok=True)
         return path
-    
-    def add(self, config:dict,data):
-        path = self._makeCache(config)
+    def add(self,data, inputData:dict = None):
+        path = self._makeCache(inputData)
         with open(path, "w") as f:
             f.write(json.dumps(data, sort_keys=True))
 
     def deleteExpiredCache(self,expireTime):
         expiredIn = self.timestamp - expireTime
         for path in self.path.iterdir():
-            timestamp = int(path.name.split('-')[0])
+            if path.is_dir():
+                continue
+            timestamp = 0
+            try:
+                timestamp = int(path.name.split('-')[0])
+            except:
+                continue
             if timestamp < expiredIn:
                 path.unlink()
-
-    def get(self,config:dict):
-        hashValue = makeHash(json.dumps(config, sort_keys=True)) + '.json'
+    def _getNewer(self, data:list):
+        selected = data[0]
+        timestamp = selected[0]
+        for actTimestamp,fileHash,matched in data:
+            if timestamp < actTimestamp:
+                timestamp = actTimestamp
+                selected = matched
+        return selected
+    
+    def _getFromPrefix(self):
         matchedFiles:list = []
+        actTimestamp = 0
+        ret = []
         for path in self.path.iterdir():
+            part = 0
             data = path.name.split('-')
+            if(len(data) == 3):
+                part = int(data.pop())
             if(len(data) != 2):
                 continue
             data[0] = int(data[0])
             timestamp,fileHash = data
-            if hashValue == fileHash:
-                matchedFiles.append([timestamp,fileHash,path])
-        selected =None
+            if self.prefix == fileHash and timestamp >= actTimestamp:
+                if timestamp > actTimestamp:
+                    actTimestamp = timestamp
+                    matchedFiles = []
+                matchedFiles.append([part, path])
         if len(matchedFiles) == 0:
             return None
-        timestamp = matchedFiles[0][0]
-        for actTimestamp,fileHash,matched in matchedFiles:
-            if not selected:
-                selected = matched
-            elif timestamp < actTimestamp:
-                timestamp = actTimestamp
-                selected = matched
-        if not selected:
-            return None
-        data = json.loads(selected.read_text())
-        return data
+        matchedFiles.sort(key=lambda x: x[0])
+        for _,path in matchedFiles:
+            data = json.loads(path.read_text())
+            ret.extend(data)
+        return ret
+    def get(self,inputData:dict = None):
+        if not inputData:
+            return self._getFromPrefix()
