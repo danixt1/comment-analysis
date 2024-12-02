bbf4bafbff32014b4a34c67feceecadd69ea0b2d
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-12-02 16:04:07 -0300

    :card_file_box: raw: small changes.
    - limit of comments is 100 if not passed in makeCSV.
    - makeData now respect the limit.
    - output has padronized to work with CommentScorer.

--- src/datasets/makeDataset.py
+++ src/datasets/makeDataset.py
@@ -11,43 +11,52 @@ MATCH_FULL_POSITIONS = r"\[[^\]]+\]"
 MATCH_VARIATIONS = r"[^|]+"
 
 
-def makeData(limit:int,exclude_tags = []):
+def makeData(limit:int,exclude_tags = [],searchIn = commentsData):
     counts ={'positive':0, 'negative':0,'-':0}
+    totalCount = 0
+    stopProcess = False
     maxPositives = int(limit * 0.70)
-    searchIn = commentsData
     comments = []
-    addComment = lambda msg,commentType,spam,behavior: comments.append(
+    addComment = lambda data,msg = None: comments.append(
         {
-            'message':msg, 
-            'type':commentType, 
-            'process':{
-                "name":"auto-generated",
-                'data':{'spam':spam, 'behavior':behavior}
-                }
+            'message':data['message'] if msg is None else msg, 
+            'type':data['type'] if 'type' in data else 'comment', 
+            'origin':'test',
+            'expected':{
+                'spam':data['spam'] if 'spam' in data else False,
+                'behavior':data['behavior'] if 'behavior' in data else '-',
+                'min_problems':data['min_problems'] if 'min_problems' in data else 0
+            }
         })
     def addToCount(behavior):
+        nonlocal totalCount, stopProcess
         if behavior == 'positive' and counts['positive'] >= maxPositives:
             return False
+        if totalCount >= limit:
+            stopProcess = True
+            return False
         counts[behavior] += 1
+        totalCount += 1
         return True
     def fromMessages(c):
         for message in c['messages']:
             variations = getVariations(message)
             for variation in variations:
+                
+                if stopProcess:
+                    return
                 if not addToCount(behavior):
                     break
-                addComment(variation, commentType, spam, behavior)
+                addComment(c,variation)
 
     for c in searchIn:
         behavior = c['behavior']
-        commentType = c['type'] if 'type' in c else 'comment'
-        spam = c['spam'] if 'spam' in c else False
+        if stopProcess:
+            break
         if 'message' in c:
-            message = c['message']
             if addToCount(behavior):
-                addComment(message,commentType,spam,behavior)
-            continue
-        if 'messages' in c:
+                addComment(c)
+        elif 'messages' in c:
             fromMessages(c)
             continue
         raise Exception(f"Unknown comment type: {c}")
@@ -71,12 +80,12 @@ def getVariations(text:str,combinations = None):
         texts.append(variation)
     return texts
 
-def makeCSV(outFile:str,config:dict = {}):
+def makeCSV(outFile:str,config:dict = {'limit':100},useData = commentsData):
     import csv
-    data = makeData(**config)
+    data = makeData(**config,searchIn=useData)
     with open(outFile, 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=['message', 'type'])
+        writer = csv.DictWriter(csvfile, fieldnames=['message', 'type','origin'])
         writer.writeheader()
         for row in data:
-            del row['process']
+            del row['expected']
             writer.writerow(row)
