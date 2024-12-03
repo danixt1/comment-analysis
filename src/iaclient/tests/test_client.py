d9e5209cd5e2d9f55a0feb3c6d742b30cd5eb110
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-12-03 11:15:47 -0300

    :white_check_mark: test: added tests to score system

--- src/iaclient/tests/test_client.py
+++ src/iaclient/tests/test_client.py
@@ -1,37 +1,60 @@
 from ..promptInfo import PromptInfo, setPromptsPath
 from ..client import IAClient
-from ...comment import Comment
+from ...comment import Comment,CommentScorer
 from ..requestProcess import RequestProcess
 from typing import List
 
 class FakeClient(IAClient):
 
-    def __init__(self):
+    def __init__(self,retData):
         super().__init__("test-client")
         self.countReqs = 0
-
+        self.expectedLot = [0,0]
+        self.actPrompt =0
+        self.retData = retData
+        self.comments = None
     def _separateCommentsBatch(self,comments: List[Comment]) -> List[List[Comment]]:
+        self.comments = comments
         lot1 = comments[::2]
         lot2 = comments[1::2]
+        self.expectedLot[0] =len(lot1)
+        self.expectedLot[1] = len(lot2)
         return [lot1,lot2]
     def _generatePrompt(self,comments: List[Comment]):
-        assert len(comments) == 3, "expected only 3 comments after being separated"
+        assert len(comments) == self.expectedLot[self.actPrompt], "Don't have the expected quantity of comments to the lot"
+        self.actPrompt+=1
         firstCmt = comments[0]
         return PromptInfo('test1' if str(firstCmt) == 'prompt1' else 'test2')
     def _makeRequestToAi(self,prompt:str,request:RequestProcess):
         self.countReqs+=1
-        if(self.countReqs > 2):
-            raise Exception('Expected only 2 requests to AI')
-        assert prompt.startswith('<FIRST_TEST>') if self.countReqs == 1 else prompt.startswith('<SECOND_TEST>')
-        request.setData([{"behavior":"neutral"},{"behavior":"happy"},{"behavior":"angry"}]).setTokensInput(2).setTokensOutput(5)
+        request.setData(self.retData).setTokensInput(2).setTokensOutput(5)
+
 def test_client_flux():
     setPromptsPath("src/iaclient/tests/prompt")
-    client = FakeClient()
+    client = FakeClient([{"behavior":"neutral"},{"behavior":"happy"},{"behavior":"angry"}])
     comments = [Comment('def','comment','test') for x in range(6)]
-    comments[0].message = "prompt1"
     result = client.analyze(comments)
+    assert client.countReqs == 2
     assert len(result['requests']) == 2
     assert len(result['batchs']) == 2
     assert result['tokens']['output'] == 10
     assert result['tokens']['input'] == 4
     assert client.countReqs == 2
+
+def test_client_with_auto_test():
+    setPromptsPath("src/iaclient/tests/prompt")
+    rets = [
+        {"behavior":"neutral"},{"behavior":"happy"},{"behavior":"angry"},
+    ]
+    TOTAL_COMMENTS_PASSED = 4
+    TOTAL_AUTO_TESTS_PERCENTAGE = 0.50
+    TOTAL_COMMENT_SCORER_EXPECTED = 2
+    TOTAL_COMMENTS_EXPECTED = TOTAL_COMMENTS_PASSED + (TOTAL_AUTO_TESTS_PERCENTAGE * TOTAL_COMMENTS_PASSED)
+    client = FakeClient(rets)
+    comments = [Comment('def', 'comment', 'test') for x in range(TOTAL_COMMENTS_PASSED)]
+    client.useAutoTest(TOTAL_AUTO_TESTS_PERCENTAGE)
+    result = client.analyze(comments)
+    assert client.countReqs == 2
+    assert len(client.comments) == TOTAL_COMMENTS_EXPECTED
+    assert len([x for x in client.comments if isinstance(x,CommentScorer)]) == TOTAL_COMMENT_SCORER_EXPECTED
+    assert 'score' in result
