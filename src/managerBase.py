acd94e49920582ce36e57dd665709943f3227808
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-12-03 12:43:39 -0300

    :card_file_box: raw: added beforeCreate and afterCreate

--- src/managerBase.py
+++ src/managerBase.py
@@ -18,13 +18,19 @@ class ManagerBase(ABC):
 
     def add(self, obj):
         self._items.append(obj)
-    
+    @classmethod
+    def _beforeCreate(cls,config:dict):
+        pass
+    @classmethod
+    def _afterCreate(cls,item,config:dict,data:dict):
+        pass
     @classmethod
     def addInstanceable(cls,name, instanceable):
         cls._builders[name] = (lambda config: instanceable(**config)) if isinstance(instanceable,type) else instanceable
     
     @classmethod
     def initWithConfig(cls,config,cache:Cache = None):
+        initData = {}
         if not 'data' in config:
             raise KeyError('data not found in config')
         data = config['data']
@@ -34,14 +40,20 @@ class ManagerBase(ABC):
         instance = cls(**config)
         for instanceConfig in data:
             if not isinstance(instanceConfig, dict):
-                raise TypeError('data must be a object')
+                raise TypeError('data must be a list')
             raiseInvalStr(instanceConfig, 'name')
             name = instanceConfig['name'].lower()
+            
+            toPut = cls._beforeCreate(instanceConfig)
+            if toPut:
+                initData = initData | toPut
             del instanceConfig['name']
 
             if not name in cls._builders:
                 raise KeyError(f'instance "{name}" not found in instanceables list, supported:{",".join(cls._builders.keys())}.')
+            
             item = cls._builders[name](instanceConfig)
+            cls._afterCreate(item,instanceConfig,initData)
             if cache != None:
                 item._cache = cache.cacheWithPrefix(instanceConfig)
             instance.add(item)
