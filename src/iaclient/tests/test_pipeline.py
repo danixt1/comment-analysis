1b3256e8ed7d5510f37ae7914c5febc865e70b45
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-12-12 20:07:13 -0300

    :white_check_mark: test: new tests added to the pipeline

--- src/iaclient/tests/test_pipeline.py
+++ src/iaclient/tests/test_pipeline.py
@@ -1,19 +1,25 @@
 from src.iaclient.pipeline import PipeRunner,Pipe
 
-def test_simplerunner():
+def test_argsPassing():
     runner = PipeRunner()
-    runned = [False,False,False]
-    def first():
+    runned = [False,False,False,False]
+    def first(*args):
+        assert len(args) == 0
         runned[0] = True
-    def second():
+    def second(**kwargs):
+        assert len(kwargs) == 0
         runned[1] = True
-    def third():
+    def third(test):
+        assert test == 11
         runned[2] = True
-    pipeRules = Pipe('test').add(first).add(second).add(third)
+    def fourth():
+        runned[3] = True
+    pipeRules = Pipe('test').add(first,second,third,fourth)
     runner.addPipe(pipeRules)
+    runner.data['test'] = 11
     runner.execute()
     assert all(runned)
-    assert [x.__name__ for x in runner.executionList] == ['first','second','third']
+    assert [x.__name__ for x in runner.executionList] == ['first','second','third','fourth']
 
 def test_beforeRule():
     runner = PipeRunner()
@@ -21,8 +27,48 @@ def test_beforeRule():
         pass
     def other():
         pass
-    pipe1 = Pipe('test').add(first)
+    def beforeOther():
+        pass
+    def trueFirst():
+        pass
+    pipe1 = Pipe('test').add(trueFirst).add(first)
     pipe2 = Pipe('test2').before('first',other)
-    runner.addPipe(pipe1).addPipe(pipe2)
-    runner.execute()
-    assert ['other','first'] == [x.__name__ for x in runner.executionList]
+    pipe3 = Pipe('test3').before('other', beforeOther)
+
+    runner.addPipe(pipe1).addPipe(pipe3).addPipe(pipe2).execute()
+
+    assert ['trueFirst','beforeOther','other','first'] == [x.__name__ for x in runner.executionList]
+
+def test_afterRule():
+    runner = PipeRunner()
+    def first():
+        pass
+    def other():
+        pass
+    def afterOther():
+        pass
+    def trueFirst():
+        pass
+    pipe1 = Pipe('test').add(trueFirst).add(first)
+    pipe2 = Pipe('test2').after('first', other)
+    pipe3 = Pipe('test3').after('other', afterOther)
+
+    runner.addPipe(pipe2).addPipe(pipe3).addPipe(pipe1).execute()
+
+    assert ['trueFirst','first','other','afterOther'] == [x.__name__ for x in runner.executionList]
+
+def test_afterWithBefore():
+    runner = PipeRunner()
+    def first():
+        pass
+    def other():
+        pass
+    def afterOther():
+        pass
+    def trueFirst():
+        pass
+    pipe1 = Pipe('test').add(trueFirst,first)
+    pipe2 = Pipe('test2').after('first', other).before('other', afterOther)
+
+    runner.addPipe(pipe2).addPipe(pipe1).execute()
+    assert ['trueFirst','first','afterOther','other'] == [x.__name__ for x in runner.executionList]
