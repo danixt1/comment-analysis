6d1ad2d5946769a1d9dfb3921884343093525c27
Daniel silva rocha <danielddsilva2@hotmail.com>
2024-12-26 19:36:07 -0300

    :white_check_mark: test: new/changes to makeDataset

--- src/tests/test_makeDataset.py
+++ src/tests/test_makeDataset.py
@@ -13,10 +13,10 @@ def test_make_all_variations_from_data():
     assert res == ['excelent test1','excelent test2','good']
 
 def test_make_dataset_with_limit():
-    res = makeData(4,searchIn=[{
+    res = makeData(10,searchIn=[{
         "lang":"en",
         "tags":["product","generic"],
-        "messages":["excelent [test1|test2|another|other] and [have this|and this| and that]",'good'],
+        "messages":["excelent [test1|test2|another|other|other initial] and [have this|and this| and that]",'good'],
         "behavior":"positive"
     },
     {
@@ -24,8 +24,27 @@ def test_make_dataset_with_limit():
         "tags":["product","generic"],
         "messages":["bad [test|test2|test3]",'bad quality',],
         "behavior":"negative"
-    }])
-    assert len(res) == 4
+    }],maxPositives=0.7)
+    assert len(res) == 10
+    assert len([x for x in res if x['expected']['behavior'] == 'positive']) == 7
+    assert len([x for x in res if x['expected']['behavior'] == 'negative']) == 3
+
+def test_tag_system():
+    res = makeData(10,searchIn=[{
+        "lang":'en',
+        "tags":["no"],
+        "messages":["NO [a1|a2|a3] and [b1|b2|b3]"],
+        "behavior":"negative"
+    },
+    {
+        "lang":"en",
+        "tags":['ok'],
+        "message":"OK",
+        "behavior":"negative"
+    }],exclude_tags=['no'])
+    assert len(res) == 1
+    assert res[0]['message'] == 'OK'
+
 def test_make_csv(tmp_path_factory):
     tmp_path = tmp_path_factory.mktemp("data")
     makeCSV(str(tmp_path / 'data.csv'),useData=[{
