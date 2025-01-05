b1bd3986fd543ba99ad2f9b3f1dea9eb7a9832ce
Daniel silva rocha <danielddsilva2@hotmail.com>
2025-01-04 22:03:53 -0300

    :recycle: refactor: padronized iter date system

--- src/output/graphics/intervalMaker.py
+++ src/output/graphics/intervalMaker.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import calendar
-from dateutil.rrule import rrule, MONTHLY
+from dateutil.rrule import rrule, MONTHLY,WEEKLY,DAILY,YEARLY
 
 class DateInterval():
 
@@ -9,119 +9,30 @@ class DateInterval():
         self._argsLegend = {}
         self._dates = dataWithTimestamp
         self._dates.sort(key=lambda x: x[0])
-        self._dates = [[datetime.fromtimestamp(x[0] / n),x[1]] for x in self._dates]
+        self._dates:list[tuple[datetime,object]] = [(datetime.fromtimestamp(x[0] / n),x[1]) for x in self._dates]
         self.intervals = []
         start = self._dates[0][0]
         end = self._dates[-1][0]
-
+        self.jumps = 1
         self.startDate:datetime = start
         self.endDate:datetime = end
 
         if granularity is None:
-            diffDays = (end - start).days
-            if diffDays < 30:
-                granularity = "day"
-            elif diffDays < 90:
-                granularity = "week"
-            else:
-                granularity = "month"
+            granularity =getGranularity(start,end)
 
         self.granularity = granularity
 
         if granularity == "day":
-            self._dailyInterval()
+            self._initIter(daiIyIter)
         elif granularity == "week":
-            self._weekInterval()
+            self._initIter(weekIter)
         elif granularity == "month":
-            self._montlyInterval()
+            self._initIter(monthIter)
         else:
             raise Exception('invalid granularity')
-        
-    def _weekInterval(self):
-        start = self.startDate
-        end = self.endDate
 
-        actMonth = start.month
-        actYear = start.year
-
-        currentWeeks =calendar.monthcalendar(actYear, actMonth)
-
-        startPosition = 0
-        for week in currentWeeks:
-            if start.day in week:
-                break
-            startPosition += 1
-        currentWeeks = currentWeeks[startPosition:]
-
-        totalMonths = (end.year - start.year) * 12 + (end.month - start.month)
-        lastData = self._stepWeek( actYear,actMonth, currentWeeks, 0)
-        for _ in range(totalMonths):
-            actMonth += 1
-            if actMonth > 12:
-                actMonth = 1
-                actYear += 1
-
-            currentWeeks =calendar.monthcalendar(actYear, actMonth)
-            continueFromLastWeek = currentWeeks[0][0] == 0
-            lastData = self._stepWeek(actYear,actMonth,currentWeeks,lastData,continueFromLastWeek)
-
-        removeLastClearIndex = len(self.intervals)
-        for i in range(len(self.intervals)-1,-1,-1):
-            if len(self.intervals[i]) == 0:
-                removeLastClearIndex = i
-            else:
-                break
-        self.intervals = self.intervals[:removeLastClearIndex]
-    
-    def _stepWeek(self,actYear,actMonth,currentWeeks,lastData, continueFromLastWeek = False):
-            lenWeeks = len(currentWeeks)
-            startFrom = len(self.intervals)
-            if continueFromLastWeek:
-                lenWeeks -=1
-                startFrom -=1
-            lastDataPos = lastData
-            self.intervals.extend([[] for _ in range(lenWeeks)])
-
-            for dataIndex in range(lastData, len(self._dates)):
-                date,data = self._dates[dataIndex]
-                if date.month == actMonth and date.year == actYear:
-
-                    for weekIndex in range(lenWeeks):
-                        if date.day in currentWeeks[weekIndex]:
-                            finalPos = weekIndex + startFrom
-                            self.intervals[finalPos].append(data)
-                            lastDataPos =  dataIndex + 1
-                            break
-                else:
-                    return lastDataPos
-            return lastDataPos
-                
-    def _dailyInterval(self):
-        start = self.startDate
-        end = self.endDate
-        daysPassed = (end - start).days
-        self.intervals = [[] for _ in range(daysPassed)]
-
-        for date,comment in self._dates:
-            diffFromStart = (date - start).days
-            self.intervals[diffFromStart].append(comment)
-        
-    def _montlyInterval(self,jumps = 1):
-        lastPassedDateIndex = 0
-
-        for date in rruleMonthIter(self.startDate,self.endDate,jumps):
-            lastPassedDateIndex = self._stepEveryMonth(lastPassedDateIndex, date.month)
-
-    def _stepEveryMonth(self,actPos:int,actMonth):
-        actInterval = []
-        for date,data in self._dates[actPos::]:
-            if date.month == actMonth:
-                actPos += 1
-                actInterval.append(data)
-            else:
-                break
-        self.intervals.append(actInterval)
-        return actPos
+    def _initIter(self,iterFn):
+        addValidItemsByDate( self._dates, self.intervals,iterFn, self.startDate, self.endDate, self.jumps)            
     
     def _makeIntervals(self,quant):
         self.intervals = [[] for _ in range(quant)]
@@ -133,42 +44,29 @@ class DateInterval():
         return self.intervals
     
     def getLegends(self):
-        return LegendInterval(self,**self._argsLegend).getLegends()
+        return LegendInterval(self,**self._argsLegend,granularity=self.granularity).getLegends()
 
 def add0(num):
     return "0" + str(num) if num < 10 else str(num)
 
 class LegendInterval():
 
-    def __init__(self, dateInterval:DateInterval,limit = 12,minLegends = 6):
+    def __init__(self, dateInterval:DateInterval,limit = 12,minLegends = 6,granularity:str = None):
         self.legends = []
         self.intervals = dateInterval.intervals
         self.startDate = dateInterval.startDate
         self.endDate = dateInterval.endDate
+        self.granularity = getGranularity(self.startDate,self.endDate) if not granularity else granularity
 
-        startDate= self.startDate
-        endDate = self.endDate
-
-        diffYears = endDate.year - startDate.year
-
-        def checkOthers():
-            totalMonths = endDate.month - startDate.month + 1
-            if totalMonths > minLegends:
-                self._legendMonth(jumpMonths = totalMonths // limit)
-            else:
-                totalDays = (endDate - startDate).days
-                if totalDays > minLegends:
-                    self._legendDay()
-                else:
-                    self._legendHour()
-
-        if diffYears > 0:
-            if diffYears > minLegends:
-                self._legendYear()
-            else:
-                checkOthers()
+        granularity = self.granularity
+        if granularity == "day":
+            self._legendDay()
+        elif granularity == "week":
+            self._legendWeek()
+        elif granularity == "month":
+            self._legendMonth()
         else:
-            checkOthers()
+            raise Exception('invalid granularity')
 
     def _legendYear(self):
         startYear = self.startDate.year
@@ -179,12 +77,18 @@ class LegendInterval():
     def _legendMonth(self,jumpMonths = 1):
         legends = []
 
-        for date in rruleMonthIter(self.startDate,self.endDate,jumpMonths):
-            legends.append(f"{add0(date.month)}/{date.year}")
+        for date in monthIter(self.startDate,self.endDate,jumpMonths):
+            legends.append(date)
 
+        self.legends = legends
+    def _legendWeek(self):
+        legends = []
+        for date in weekIter(self.startDate, self.endDate, 1):
+            legends.append(date)
         self.legends = legends
 
     def _legendDay(self):
+
         startDate = self.startDate
         endDate = self.endDate
 
@@ -194,31 +98,69 @@ class LegendInterval():
         if startDate.month == endDate.month:
             self.legends = [add0(startDay + i) for i in range(endDay - startDay)]
             return
-        actDay = startDate.day
-        actMonth = startDate.month
-        totalDays = (endDate - startDate).days
-        for _ in range(totalDays):
-            self.legends.append(f"{add0(actDay)}/{add0(actMonth)}")
-            actDay += 1
-            if actDay > calendar.monthrange(startDate.year, actMonth)[1]:
-                actDay = 1
-                actMonth += 1
-                if actMonth > 12:
-                    actMonth = 1
-    
-    def _legendHour(self):
-        startHour = self.startDate.hour
-        endHour = self.endDate.hour
-        self.legends = [add0(startHour + i) for i in range(endHour - startHour)]
+        for date in daiIyIter(startDate, endDate, 1):
+            self.legends.append(f"{add0(date.day)}/{add0(date.month)}")
 
     def getLegends(self):
         return self.legends
+def getGranularity(start,end):
+    diffDays = (end - start).days
+    if diffDays < 30:
+        return "day"
+    elif diffDays < 90:
+        return "week"
+    else:
+        return "month"
 
-def rruleMonthIter(start,end,intervals):
-    # Solution to RFC 3.3.10 more in: https://dateutil.readthedocs.io/en/stable/rrule.html
+def addValidItemsByDate(fromDateObjects,insertIn:list,iterFn,startDate,EndDate,jumps = 1):
+        lastPos = 0
+        for date in iterFn(startDate, EndDate,jumps):
+            toInsert = []
+            actDate,item = fromDateObjects[lastPos]
+            if actDate > date:
+                insertIn.append(toInsert)
+                continue
+            while lastPos < len(fromDateObjects):
+                actDate,item = fromDateObjects[lastPos]
+                if actDate > date:
+                    break
+                toInsert.append(item)
+                lastPos += 1
+            insertIn.append(toInsert)
+    
+def daiIyIter(start,end,jumpDays):
+    startDate = datetime(start.year, start.month, start.day)
+    endDate = datetime(end.year, end.month, end.day)
+    actJump = 0
+    for date in rrule(freq=DAILY, dtstart=startDate, until=endDate):
+        actJump += 1
+        if actJump == jumpDays:
+            actJump = 0
+        else:
+            continue
+        yield datetime(date.year, date.month, date.day, 23, 59, 59)
+def weekIter(start,end,jumpWeeks):
+    def addWeekDays(date):
+        return  6 - calendar.weekday(date.year,date.month,date.day)
+    startDate = datetime(start.year, start.month, start.day)
+    endDate = datetime(end.year, end.month, end.day) + timedelta(days= addWeekDays(end))
 
-    startDate = datetime(start.year, start.month, 1, 2)
-    endDate = datetime(end.year, end.month, 1, 2)
+    for date in rrule(freq=WEEKLY, dtstart=startDate, until=endDate, interval=jumpWeeks):
+        yield datetime(date.year, date.month, date.day, 23, 59, 59) + timedelta(days=addWeekDays(date))
 
-    for date in rrule(freq=MONTHLY, dtstart=startDate, until=endDate, interval=intervals):
-        yield date
+def monthIter(start,end,jumpMonths):
+    # set the initial data to day 1 to avoid skip of date. See (RFC 3.3.10): https://dateutil.readthedocs.io/en/stable/rrule.html
+    startDate = datetime(start.year, start.month, 1)
+    endDate = datetime(end.year, end.month, 1)
+
+    for date in rrule(freq=MONTHLY, dtstart=startDate, until=endDate, interval=jumpMonths,):
+        yield datetime(date.year,date.month,getLastDayOfMonth(date),23,59,59)
+
+def yeaIter(start, end,jump):
+    startDate = datetime(start.year, 1, 1, 2)
+    endDate = datetime(end.year, 1, 1, 2)
+
+    for date in rrule(freq=YEARLY, dtstart=startDate, until=endDate):
+        yield datetime(date.year, 12, 31, 23, 59, 59)
+def getLastDayOfMonth(date):
+    return calendar.monthrange(date.year, date.month)[1]
