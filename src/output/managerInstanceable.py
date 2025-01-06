eee0b97a73c024248778f1a931ad81f99fd1e927
Daniel silva rocha <danielddsilva2@hotmail.com>
2025-01-06 19:01:45 -0300

    :sparkles: feat: added Graphic output

--- src/output/managerInstanceable.py
+++ src/output/managerInstanceable.py
@@ -3,5 +3,7 @@ from src.managerBase import ManagerBase
 def initInstanceables(managerClass:ManagerBase):
 
     from .outputMongoDb import OutputMongoDb
-
+    from .outputGraphic import OutputGraphic
+    
+    managerClass.addInstanceable('graphic',OutputGraphic)
     managerClass.addInstanceable('mongodb',OutputMongoDb)
